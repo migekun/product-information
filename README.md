88db25e01cd0aa911a9179b794e66dc26431590e
mimikun <mimikun@192.168.0.159>
2017-10-15 09:31:59 +0200

    Tomcat support and war generation added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# product-information
+# Product Information
 Spring boot test, Spring MVC, thymeleaf test for a list of products
+
+- Generate a war executing mvn package (optional -DskipTests)
+- Deploy to tomcat
+- Browser URL: http://localhost:8080/productList/
+
--- src/main/java/org/coding/test/ProductInformationApplication.java
+++ src/main/java/org/coding/test/ProductInformationApplication.java
@@ -2,10 +2,17 @@ package org.coding.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductInformationApplication {
+public class ProductInformationApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProductInformationApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProductInformationApplication.class, args);
 	}
